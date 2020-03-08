e8c24f64de50286c616456e54f920abb32937fe8
Leein <leein1986@foxmail.com>
2020-03-08 09:43:15 +0800

    dd

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+// 这是项目的根组件
+
+import React, { Component } from 'react';
+
+// 导入路由组件
+//import { HashRouter, Route, Link } from 'react-router-dom'
+
+// 导入需要的 Ant Design 组件
+// import { Layout, Menu } from 'antd';
+// const { Header, Content, Footer } = Layout;
+// 导入模块化的样式
+// import styles from './css/app.scss'
+
+// 导入 路由相关的组件页面
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return 
+      <div>
+        <p>
+          star-an
+        </p>
+      </div>
+    
+  }
+}
